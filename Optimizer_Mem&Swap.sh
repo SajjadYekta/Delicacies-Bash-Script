8af6a893cfc88e2f956ecdcb74b4700bcaed5bf5
SajjadYekta <sajjad,fallahyekta@gmail.com>
2023-10-27 21:40:57 +0330

    edited

--- Optimizer_Mem&Swap.sh
+++ Optimizer_Mem&Swap.sh
@@ -12,11 +12,11 @@ if [ "$SWAPPINESS" != "$SWAP_SUGGEST" ]; then
     SWAPP_SYS_CONF=$(grep "vm.swappiness" /etc/sysctl.conf | awk -F "_" '{print $3}')
         if [ -z "$SWAPP_SYS_CONF" ]; then
             echo 'vm.swappiness='$SWAP_SUGGEST >> /etc/sysctl.conf
-            sysctl --system > /dev/null 3>&1
+            sysctl --system > /dev/null 2>&1
             echo The system use Swapp when Ram usage upper than $RAM_USAGE%
         else
             sed -i -e 's/vm.swappiness.*/vm.swappiness='"$SWAP_SUGGEST"'/g' /etc/sysctl.conf
-            sysctl --system > /dev/null 3>&1
+            sysctl --system > /dev/null 2>&1
             echo The system use Swapp when Ram usage upper than $RAM_USAGE%
         fi
 else
