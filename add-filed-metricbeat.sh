9b3d811dea8cd668df112b3afe97ce9b6c34c215
SajjadYekta <sajjad,fallahyekta@gmail.com>
2023-12-28 11:45:35 +0330

    first version

--- add-filed-metricbeat.sh
+++ add-filed-metricbeat.sh
@@ -1,4 +1,4 @@
-sourcexml=$(ls /appsajjad/tomcat/conf/Catalina/localhost/*.xml | tr '\n' ' ')
+sourcexml=$(ls -l /appsajjad/tomcat/conf/Catalina/localhost/*.xml | tr '\n' ' ')
 sed -i '/project_name/s/"[^"]*"/"'"$sourcexml"'"/g' /etc/metricbeat/metricbeat.yml
 if [ "$?" == "0" ]; then
     systemctl stop metricbeat.service
